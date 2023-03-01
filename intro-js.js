4e14b73e248bc332bc7d2b78cdbc12c49b91a33b
Alyssa Cervantes (she/her) <112597248+alyssaleanne98@users.noreply.github.com>
2023-03-01 15:16:07 -0800

    variables review

--- intro-js.js
+++ intro-js.js
@@ -10,3 +10,34 @@
 
         const z = 10 
         z = 100 // uncaight TypeError: assignment to constant variable 
+
+        const person = { name: "Fred"} // objects are reference types - if we assign an object to a variable, that variable does not actually hold the object. 
+        person.age = 25 // no error
+
+        person = {name: "Barney"} // uncaught typeError
+
+
+        // Basic Document structure
+
+alert("Hello World"); // alert is a command in JS with an argument "hello world"
+
+
+var name = prompt("What is your name?")
+
+// Variables
+
+var age = 44; // name of variable storing a pos integer
+var status = -10; // we can also store neg integers
+
+var gpa = 3.45; //floating point number = decimals
+
+var output = "";
+
+output += "Age " + age;
+output += "<br/>";
+
+
+alert("Mark is " + age + " years old"); // this is called concatenation when we are adding all strings 
+
+
+
